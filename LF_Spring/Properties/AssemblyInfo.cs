feaaed8ff6397f65ecc309790d61618b950697b6
wangdong3 <wangdong3@beisen.com>
2019-08-29 16:23:56 +0800

    修改项目结构

--- LF_Spring/Properties/AssemblyInfo.cs
+++ LF_Spring/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Test.Framework")]
+[assembly: AssemblyTitle("LF_Spring")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("LF_Spring")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("c4666b99-fb69-4534-9747-9c32ddc02679")]
+[assembly: Guid("e2b20a88-a7d1-48f8-8eb8-d703a5137ad2")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
